f342474614eb3da4cdec3c9df7f3b0363d912237
Daneo Van Overloop <daneo.vanoverloop@unleashed.be>
2017-12-07 14:57:17 +0100

    Now showing if dependency is being skipped

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -e Podfile.lock ] && pod install
+[ -e Podfile.lock ] && pod install || echo "No Podfile.lock, skipping..."
 [ -d Dependencies ] && cd Dependencies
-[ -e Gemfile.lock ] && bundle install --binstubs
-[ -e Cartfile.resolved ] && carthage bootstrap --platform 'ios,watchos' --cache-builds
+[ -e Gemfile.lock ] && bundle install --binstubs || echo "No Gemfile.lock, skipping..."
+[ -e Cartfile.resolved ] && carthage bootstrap --platform 'ios,watchos' --cache-builds || echo "No Cartfile.resolved, skipping..."
